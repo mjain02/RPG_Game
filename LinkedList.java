63d14234a37d649961178532d2714a450b8791b1
Meghna Jain <meghna.jain2612@gmail.com>
2022-03-14 01:12:03 -0400

    Embedded Systems Game

---
+++ LinkedList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class LinkedList implements Comparable<T> {
+    private Node<T> head;
+    private int size;
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+}
